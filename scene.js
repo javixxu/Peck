c6c35d8331650119bd6bb381474ce35f1f891f98
admont02 <admont02@ucm.es>
2021-10-26 17:12:01 +0200

    plataformas ajustadas para que llegue cathy cuajada

--- scene.js
+++ scene.js
@@ -27,8 +27,8 @@ export default class Level extends Phaser.Scene {
     this.bases = this.add.group();
     this.player = new Player(this, 200, 300);
    
-    new Platform(this, this.player, this.bases, 150, 350);
-    new Platform(this, this.player, this.bases, 850, 350);
+    new Platform(this, this.player, this.bases, 150, 450);
+    new Platform(this, this.player, this.bases, 850, 450);
     //new Platform(this, this.player, this.bases, 500, 200);
     //new Platform(this, this.player, this.bases, 150, 100);
     //new Platform(this, this.player, this.bases, 850, 100);
