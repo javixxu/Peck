cadfdb16e5075ef81e4aa2d1e141fc33a8fbc892
sarais02 <sarais02@ucm.es>
2021-11-02 01:35:33 +0100

    xr7i5

--- src/game.js
+++ src/game.js
@@ -2,7 +2,8 @@ let config = {
     type: Phaser.AUTO,
     width:  1000,
     height: 500,
-    backgroundColor: '392542',
+    backgroundColor:'000000',
+    //backgroundColor: '392542',
     scale: {
         // mode: Phaser.Scale.FIT,  
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
