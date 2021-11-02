44e5a8949cfa9f0154048a4ad50c2f6f7306f93d
sarais02 <sarais02@ucm.es>
2021-11-02 01:37:54 +0100

    ·%y

--- src/game.js
+++ src/game.js
@@ -2,14 +2,14 @@ let config = {
     type: Phaser.AUTO,
     width:  1000,
     height: 500,
-    backgroundColor:'000000',
+    backgroundColor:'111111',
     //backgroundColor: '392542',
     scale: {
         // mode: Phaser.Scale.FIT,  
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
     },
     pixelArt: true,
-    scene: [Boot, Level, End],
+    //scene: [Boot, Level, End],
     physics: { 
         default: 'arcade', 
         arcade: { 
