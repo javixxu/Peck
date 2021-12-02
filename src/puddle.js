2ff3567477286fb6a452bee8c03cfa3f75ab362d
admont02 <admont02@ucm.es>
2021-12-02 22:46:05 +0100

    efecto del puddle se hace en su script

--- src/puddle.js
+++ src/puddle.js
@@ -5,4 +5,25 @@ export default class Puddle extends Obstacles
     constructor(scene, player, x, y, name) {
         super(scene, player, x, y, name);
     }
-}
+    preUpdate() {
+     
+        super.preUpdate();
+        
+         if (this.scene.physics.overlap(this.scene.player, this)) 
+        {
+            this.effect();
+        }
+         
+        }
+        effect(){
+            this.scene.player.speed = 200;
+            this.scene.player.jumpSpeed = -250;
+        
+            let timer=this.scene.time.addEvent( {
+              delay:5000,
+              callback: this.scene.player.setSpeed,
+              callbackScope: this.scene.player
+            });
+        }
+        
+      }
---
+++ src/spikes.js
@@ -0,0 +1,8 @@
+import Obstacles from "./obstacles.js";
+
+export default class Spikes extends Obstacles
+{
+    constructor(scene, player, x, y, name) {
+        super(scene, player, x, y, name);
+    }
+}
