b8600934dd308e3b0f638fc3f51c24963858b20e
admont02 <admont02@ucm.es>
2021-12-03 13:10:19 +0100

    los pinchos estan terminados, pero tenemos que arreglar el timer (es el mismo que el de los enemigos)porque sino solo funcionan correctamente con el primer choque

--- src/spikes.js
+++ src/spikes.js
@@ -4,5 +4,21 @@ export default class Spikes extends Obstacles
 {
     constructor(scene, player, x, y, name) {
         super(scene, player, x, y, name);
+        
     }
-}
+    preUpdate() {
+     
+        super.preUpdate();
+        
+         if (this.scene.physics.overlap(this.scene.player, this)) 
+        {
+            this.effect();
+        }
+         
+        }
+        effect(){
+            this.scene.player.PerderVida(0.5);
+            this.scene.player.changeInvulnerability()
+        }
+        
+      }
