73eff8d4906aeed2e914983ff4dba9387c5d2f4d
admont02 <admont02@ucm.es>
2021-12-19 15:37:37 +0100

    periquito casi implementado

--- src/birds/enemies.js
+++ src/birds/enemies.js
@@ -12,6 +12,7 @@ export default class Enemies extends  Phaser.GameObjects.Sprite
         this.speedPos = 200;
         this.speedInv=-200;
         this.speed = -200;
+        this.destroyed=false;
     }
 
     collision()
@@ -19,5 +20,14 @@ export default class Enemies extends  Phaser.GameObjects.Sprite
         this.setActive(false);
         this.setVisible(false);
         console.log("DESTRUIDO");
+    }
+    preUpdate(t, dt){
+        super.preUpdate(t,dt);
+        if(this.scene.physics.overlap(this,this.scene.trigger)){
+           this.setActive(false);
+           this.setVisible(false);
+           this.destroy();
+           this.destroyed=true;
+        }
     }
 }
